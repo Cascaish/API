a2840487ffc6ddf73f8d0021831ab75c4efc52ea
Cascaish <ra54269926813@acaonsfatima.org.br>
2024-08-02 09:38:36 -0300

    subindo

--- app.js
+++ app.js
@@ -19,21 +19,3 @@ servidor.listen(
     5001,
     () => console.log('API 5001')
 )
-
-
-servidor.post ('/dobro', (req,resp) =>{
-    let n1 = Number(req.body.n1);
-    let n2 = Number(req.body.n2);
-    let soma = n1 + n2;
-    
-    resp.send({
-        soma: soma})
-    
-    })
-    
-    
-    servidor.listen(
-    
-        5001,
-        () => console.log('API 5001')
-    )
